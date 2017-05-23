305dde2b67d91cc4309b9f2069dfd740d7808236
Riccardo <riccardo.buscaroli@studio.unibo.it>
2017-05-23 03:31:47 +0200

    qualche funzione

---
+++ MyAgente/src/it/jar/mulino/logic/Nodo.java
@@ -0,0 +1,36 @@
+package it.jar.mulino.logic;
+
+import java.util.*;
+import it.jar.mulino.model.*;
+
+public class Nodo implements Iterable<Nodo> {
+	final Stato s;
+	int upperbound,lowerbound;
+	Nodo figlio,fratello;
+	public Nodo(Stato s){
+		this.s=s;
+	}
+	/*public Stato getStato(){
+		return s;
+	}*/
+	public Nodo firstchild(){
+		return figlio;
+	}
+	public Nodo nextbrother(){
+		return fratello;
+	}
+	@Override
+	public Iterator<Nodo> iterator(){
+		return new Iterator<Nodo>(){
+			private Nodo n=figlio;
+			public Nodo next(){
+				Nodo n2=n;
+				n=n.fratello;
+				return n2;
+			}
+			public boolean hasNext(){
+				return n!=null;
+			}
+		};
+	}
+}
